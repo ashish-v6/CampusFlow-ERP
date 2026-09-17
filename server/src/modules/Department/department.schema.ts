20752f6e6ca1a0c7bc0d45eefe53ff8f6bf4b0de
ashish-v-ai <ashishvekariya165@gmail.com>
2026-09-17 17:12:15 +0530

    Update department schema

--- server/src/modules/Department/department.schema.ts
+++ server/src/modules/Department/department.schema.ts
@@ -1,5 +1,26 @@
 import { z } from "zod";
+import { DepartmentStatus } from "../../generated/prisma/enums.js";
 
-export class DepartmentSchema {}
+export class DepartmentSchema {
+  public createDepartmentSchema = z.object({
+    name: z.string().trim().min(1).max(40),
+    code: z.string().trim().min(1).max(12),
+  });
+
+  public departmentQuerySchema = z.object({
+    page: z.coerce.number().int().positive().default(1),
+    limit: z.coerce.number().int().positive().max(10).default(10),
+    search: z.string().trim().min(1).max(20).optional(),
+    status: z.enum(DepartmentStatus).optional(),
+  });
+
+  public updateDepartmentSchema = z
+    .object({
+      name: z.string().trim().min(1).max(40).optional(),
+      code: z.string().trim().min(1).max(12).optional(),
+      status: z.enum(DepartmentStatus).optional(),
+    })
+    .refine((data) => Object.keys(data).length > 0, { message: "At least one field is required" });
+}
 
 export const departmentSchema = new DepartmentSchema();
