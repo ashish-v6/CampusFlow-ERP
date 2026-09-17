e18e96b49afb28d239ba0748f15d8127edf7e05d
ashish-v-ai <ashishvekariya165@gmail.com>
2026-09-17 17:12:12 +0530

    Update department routes

--- server/src/modules/Department/department.route.ts
+++ server/src/modules/Department/department.route.ts
@@ -6,6 +6,35 @@ import { departmentSchema } from "./department.schema.js";
 
 const router = Router();
 
-router.get("/", departmentController.getDepartments);
+router.post(
+  "/",
+  authenticate,
+  authorize("admin"),
+  validateSchema(departmentSchema.createDepartmentSchema, "body"),
+  departmentController.createDepartment,
+);
+
+router.get(
+  "/:id",
+  authenticate,
+  authorize("admin"),
+  departmentController.getDepartmentById,
+);
+
+router.get(
+  "/",
+    authenticate,
+    authorize("admin"),
+  validateSchema(departmentSchema.departmentQuerySchema, "query"),
+  departmentController.getDepartments,
+);
+
+router.patch(
+  "/:id",
+  authenticate,
+  authorize("admin"),
+  validateSchema(departmentSchema.updateDepartmentSchema, "body"),
+  departmentController.updateDepartment,
+);
 
 export default router;
